4b516f0b84b13c1dde5602e20e0c49b765b4edc9
manturon <ignacio.bblanco@gmail.com>
2022-10-31 01:05:24 -0300

    Read keyboard events

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Werdel } from "./Werdel";
 
 export const App = () => {
--- src/Werdel.tsx
+++ src/Werdel.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 interface BlockValue {
   main: string;
@@ -16,7 +16,7 @@ const BLOCK_VALUES: Map<string, BlockValue> = new Map([
 ]);
 
 interface BlockProps {
-  value: BlockValue | null;
+  value: BlockValue | null | undefined;
   state?: BlockState;
 }
 
@@ -25,6 +25,7 @@ const SOME_MATCH_BG = "bg-yellow-400";
 const MATCH_BG = "bg-green-400";
 
 const Block = ({ value, state = BlockState.NoMatch }: BlockProps) => {
+  const BASE_STYLE = "rounded-md p-4 flex flex-col items-center";
   if (value) {
     let bg =
       state === BlockState.Match
@@ -33,25 +34,28 @@ const Block = ({ value, state = BlockState.NoMatch }: BlockProps) => {
         ? SOME_MATCH_BG
         : MUTED_BG;
     return (
-      <div className={`${bg}`}>
-        <span>{value.main}</span>
-        <span>{value.secondary}</span>
+      <div className={`${BASE_STYLE} ${bg}`}>
+        <b className="text-bold">{value.main}</b>
+        <small>{value.secondary}</small>
       </div>
     );
   } else {
-    return <div className={`${MUTED_BG} rounded-md p-4`}></div>;
+    return <div className={`${BASE_STYLE} ${MUTED_BG}`}></div>;
   }
 };
 
 interface RowProps extends React.PropsWithChildren {
   columns: number;
+  values: string;
 }
 
-const Row = ({ columns, children }: RowProps) => {
+const Row = ({ columns, values, children }: RowProps) => {
   if (!children) {
     let emptyBlocks = Array(columns)
       .fill(null)
-      .map((_e, index) => <Block key={index} value={null} />);
+      .map((_e, index) => (
+        <Block key={index} value={BLOCK_VALUES.get(values[index])} />
+      ));
     return <div className="flex flex-row gap-1">{emptyBlocks}</div>;
   } else {
     return <div></div>;
@@ -70,8 +74,22 @@ export const Werdel = ({
   rows = DEFAULT_ROWS,
   columns = DEFAULT_COLUMNS,
 }: WerdelProps) => {
+  let [input, setInput] = useState("");
+  let handleKeydown = ({ key }) => {
+    if (key === "Backspace") {
+      setInput((input) => (input ? input.slice(0, -1) : ""));
+    } else if (key.match(/^[A-z]$/)) {
+      setInput((input) => (input.length < rows ? input + key : input));
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, []);
   let blockRows = Array(rows)
     .fill(null)
-    .map((_e, index) => <Row columns={columns} key={index}></Row>);
+    .map((_e, index) => (
+      <Row key={index} columns={columns} values={input}></Row>
+    ));
   return <div className="flex flex-col gap-1 p-2">{blockRows}</div>;
 };
