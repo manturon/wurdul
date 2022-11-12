2e18a5ce2ece2e21493a77f20951cc9f7423a571
manturon <ignacio.bblanco@gmail.com>
2022-11-12 01:11:12 -0300

    Changed font to Fira Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
+        sans: ["Fira Sans", ...defaultTheme.fontFamily.sans],
       },
     },
   },
