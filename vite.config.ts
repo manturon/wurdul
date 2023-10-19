27e9794622a4520db5f0e0abf6fdc4532c03e4e1
manturon <ignacio.bblanco@gmail.com>
2023-10-18 23:45:49 -0300

    back to react

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import preact from "@preact/preset-vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [react()],
   test: {
-	globals: true,
-  }
+    globals: true,
+  },
 });
