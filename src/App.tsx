ce4e0f2643c9c8f930d0e425224c48699b8689ff
manturon <ignacio.bblanco@gmail.com>
2023-10-18 23:47:22 -0300

    move App out of main

---
+++ src/App.tsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import { PhonemeKey, phonemes } from "./game/english";
+
+function PhonemeBlock({ of }: { of: PhonemeKey }) {
+  const { key, ipa } = phonemes[of]!;
+  return (
+    <div className="phoneme-block">
+      <span>{ipa}</span>
+    </div>
+  );
+}
+
+function randomPhonemes(n: number) {
+  const definitions = Object.keys(PhonemeKey) as (keyof typeof PhonemeKey)[];
+  const indices = new Array(n);
+  for (const [i] of indices.entries()) {
+    indices[i] =
+      PhonemeKey[definitions[Math.floor(Math.random() * definitions.length)]!];
+  }
+  return indices as PhonemeKey[];
+}
+
+const dictionary: Record<string, string[]> = await fetch(
+  new URL("./dictionary/all.json", import.meta.url),
+).then((res) => (res.status === 200 ? res.json() : null));
+
+function lookup(key: string) {
+  const result = dictionary[key] ?? [];
+  const normalized = normalizeTextForLookup(key);
+  if (normalized !== key) {
+    const resultNormalized = normalizeTextForLookup(key);
+    if (resultNormalized) {
+      result.push(...resultNormalized);
+    }
+  }
+  return result;
+}
+
+function normalizeTextForLookup(s: string) {
+  return s.replaceAll(/['-]+/g, "");
+}
+
+function normalizeTextForInput(s: string) {
+  return s.replaceAll(/[^A-Za-z'-]+/g, "").toLowerCase();
+}
+
+export function App() {
+  const [input, setInput] = useState("");
+  const [currentChoice, setCurrentChoice] = useState(0);
+
+  let pronunciations = lookup(input);
+
+  return (
+    <div className="game">
+      <div className="blocks">
+        <div className="line">
+          {randomPhonemes(5).map((p, i) => (
+            <PhonemeBlock key={i} of={p} />
+          ))}
+        </div>
+        <div className="line">
+          {randomPhonemes(5).map((p, i) => (
+            <PhonemeBlock key={i} of={p} />
+          ))}
+        </div>
+        <div className="line">
+          {randomPhonemes(5).map((p, i) => (
+            <PhonemeBlock key={i} of={p} />
+          ))}
+        </div>
+        <div className="line">
+          {randomPhonemes(5).map((p, i) => (
+            <PhonemeBlock key={i} of={p} />
+          ))}
+        </div>
+        <div className="line">
+          {randomPhonemes(5).map((p, i) => (
+            <PhonemeBlock key={i} of={p} />
+          ))}
+        </div>
+        <div className="line">
+          {randomPhonemes(5).map((p, i) => (
+            <PhonemeBlock key={i} of={p} />
+          ))}
+        </div>
+        <hr />
+        <div className="line">
+          {Array.from(pronunciations[currentChoice] ?? "").map((p, i) => (
+            <PhonemeBlock of={p as PhonemeKey} key={i} />
+          ))}
+        </div>
+      </div>
+      <div className="input">
+        <input
+          type="text"
+          value={input}
+          onKeyDown={(event) => {
+            if (/^\d$/.test(event.key)) {
+              const n = parseInt(event.key);
+              if (Number.isFinite(n) && n > 0 && n <= pronunciations.length) {
+                setCurrentChoice(n - 1);
+              }
+            }
+          }}
+          onChange={({ target }) => {
+            if (!("value" in target) || typeof target.value !== "string") {
+              return;
+            }
+            const newValue = normalizeTextForInput(target.value);
+            if (input === newValue) {
+              return;
+            }
+            setInput(newValue);
+            setCurrentChoice(0);
+          }}
+        />
+
+        {pronunciations.length > 1 && (
+          <div className="blocks">
+            <ol>
+              {pronunciations.map((pr, i) => (
+                <li
+                  key={i}
+                  className={`choice ${i === currentChoice ? "chosen" : ""}`}>
+                  <span>{pr}</span>
+                </li>
+              ))}
+            </ol>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
